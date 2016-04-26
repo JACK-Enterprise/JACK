7bf59b8d8690a8e5173b59f667efd27ed88fc8b2
Sorion <maxime.pelte@gmail.com>
2016-04-26 13:19:01 +0200

    Adding the AboutController for the About window

--- src/com/jack/address/view/AboutController.java
+++ src/com/jack/address/view/AboutController.java
@@ -1,7 +1,48 @@
 package com.jack.address.view;
 
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import com.jack.address.MainApp;
+
 /**
  * Created by Maxime on 26/04/2016.
  */
 public class AboutController {
+
+    private Stage aboutStage;
+    private boolean clicked = false;
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private  void initialize(){
+
+    }
+
+    /**
+     * Sets the stage of this window
+     * @param aboutStage
+     */
+    public void setAboutStage(Stage aboutStage){
+        this.aboutStage = aboutStage;
+    }
+
+    /**
+     * Returns the boolean
+     * @return Returns a boolean
+     */
+    public boolean isClicked() {
+        return clicked;
+    }
+
+    /**
+     * Called when the user clicks on the window
+     */
+    @FXML
+    private void handleClick(){
+        clicked = true;
+        aboutStage.close();
+    }
 }
