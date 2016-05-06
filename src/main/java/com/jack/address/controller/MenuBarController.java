54ae88d9994e0b899d1ba6ac6b3c5c5bf412525f
Sorion <maxime.pelte@gmail.com>
2016-05-06 14:32:23 +0200

    Adding getCapabilities in the menu Edit to check any URL

--- src/main/java/com/jack/address/controller/MenuBarController.java
+++ src/main/java/com/jack/address/controller/MenuBarController.java
@@ -7,6 +7,16 @@ package com.jack.address.controller;
 import com.sun.glass.ui.MenuBar;
 import javafx.fxml.FXML;
 import com.jack.address.MainApp;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sun.security.tools.policytool.Resources;
+
+import java.util.ResourceBundle;
 
 /**
  * The controller class for the MenuBar
@@ -58,5 +68,16 @@ public class MenuBarController {
     @FXML
     private void handleExit(){
         mainApp.cancelRequest();
+    }
+
+    @FXML
+    private void handleGetCapabilities(){
+
+        GetCapabilitiesWindow getCapabilitiesWindow = new GetCapabilitiesWindow();
+
+        getCapabilitiesWindow.initView();
+
+
+
     }
 }
