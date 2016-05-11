1900b2e20d89d825105b6353ce06c9bc553a8f38
Sorion <maxime.pelte@gmail.com>
2016-05-11 17:39:13 +0200

    Updating send button controller to create a new GetCapabilities intance & display the XML file

--- src/main/java/com/jack/address/controller/GetCapabilitiesController.java
+++ src/main/java/com/jack/address/controller/GetCapabilitiesController.java
@@ -1,5 +1,6 @@
 package com.jack.address.controller;
 
+import com.jack.wms.GetCapabilities;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 
 /**
  * Created by Maxime on 11/05/2016.
@@ -36,6 +38,12 @@ public class GetCapabilitiesController {
     @FXML
     private void handleSendButton() {
         System.out.println("URL : " + textUrl.getText());
+        GetCapabilities xml = new GetCapabilities(textUrl.getText());
+        try {
+            xml.getCapabilities();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void setStage(Stage stage) {
