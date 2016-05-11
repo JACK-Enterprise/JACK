2b3781011decb4634283ba4918b90c343d5d7584
Sorion <maxime.pelte@gmail.com>
2016-05-11 17:13:24 +0200

    Updating controller when click on submit button

--- src/main/java/com/jack/address/controller/GetCapabilitiesController.java
+++ src/main/java/com/jack/address/controller/GetCapabilitiesController.java
@@ -1,7 +1,14 @@
 package com.jack.address.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
 /**
  * Created by Maxime on 11/05/2016.
@@ -11,28 +18,34 @@ public class GetCapabilitiesController {
     private Stage GetCapabilitiesStage;
     private boolean click = false;
 
+    @FXML
+    private TextField textUrl;
+
 
     @FXML
-    private void initialize(){
+    private void initialize() {
 
     }
 
     @FXML
-    private void handleCancelButton(){
+    private void handleCancelButton() {
         click = true;
         GetCapabilitiesStage.close();
     }
 
+    @FXML
+    private void handleSendButton() {
+        System.out.println("URL : " + textUrl.getText());
+    }
 
-    public void setStage(Stage stage){
+    public void setStage(Stage stage) {
 
         this.GetCapabilitiesStage = stage;
 
     }
 
-    public boolean isClick(){
+    public boolean isClick() {
         return click;
     }
-
 
 }
