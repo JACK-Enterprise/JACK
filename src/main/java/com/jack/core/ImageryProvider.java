6b150e5d2757925302bdafc60f765fb51fcaa722
Sorion <maxime.pelte@gmail.com>
2016-05-18 15:26:07 +0200

    removing class URLTemplateImagery and define member and method for interface ImageryProvider

--- src/main/java/com/jack/core/ImageryProvider.java
+++ src/main/java/com/jack/core/ImageryProvider.java
@@ -4,4 +4,14 @@ package com.jack.core;
  * Created by Maxime on 17/05/2016.
  */
 public interface ImageryProvider {
+
+    int minimumLevel();
+    int maximumLevel();
+    int tileHeight();
+    int tileWidth();
+    Rectangle rectangle();
+
+    void loadImage(String url);
+
+
 }
