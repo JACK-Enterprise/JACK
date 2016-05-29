1cf829642c2b3510c79683bef0ea5e1448ccb2df
ACID-KILLA666 <aurelien.duval6@gmail.com>
2016-05-29 15:29:53 +0200

    Rendered plugins runnable

--- src/main/java/com/jack/plugins/manager/Plugin.java
+++ src/main/java/com/jack/plugins/manager/Plugin.java
@@ -2,13 +2,27 @@ package com.jack.plugins.manager;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  *
  * @author Aurelien
  */
 @AllArgsConstructor
+@Slf4j
 public class Plugin {
     @Getter private PluginBase plugin;
     @Getter private PluginDescription description;
+    
+    public Object run(Object ... args) {
+        String name = description != null ? description.getName() : "null";
+        if(plugin != null)
+        {
+            log.trace("Running \"" + name + "\" plugin");
+            return plugin.run(args);
+        }
+        
+        log.warn("Plugin \"" + name + "\" is not correctly set");
+        return null;
+    }
 }
